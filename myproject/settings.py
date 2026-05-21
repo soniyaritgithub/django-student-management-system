92af109d0b972358e92211f193bf15cb08ed8574
sunidhi shinde <sunidhishinde28@gmail.com>
2026-05-21 12:08:40 +0530

    Switched to SQLite for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'studentdb',
-        'USER': 'root',
-        'PASSWORD': 'root123',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
