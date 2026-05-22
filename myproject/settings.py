62152a6537ff6a67fbf92c136cc5d0f5ba600120
sunidhi shinde <sunidhishinde28@gmail.com>
2026-05-22 16:01:03 +0530

    Fixed Vercel CORS settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -165,6 +165,14 @@ LOGIN_REDIRECT_URL = '/'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+
+    "https://django-student-management-system-chi.vercel.app",
+
+]
+
 
 # REST FRAMEWORK + JWT
 
